61065b287de4a19bca6fc36ccfd2247bc5c2c7c6
Oleksii Pudlo <pudloaleksey@gmail.com>
2024-12-08 16:25:09 +0100

    feat: enhance playwright configuration with viewport and launch options

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,24 +13,35 @@ export default defineConfig({
   ],
   use: {
     baseURL: URLS.BASE_URL,
+    viewport: { width: 1900, height: 1200 },
+    launchOptions: {
+      slowMo: 300,
+    },
+    headless: true,
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
   },
   projects: [
     {
-      name: 'Chromium',
+      name: 'dev',
       use: { ...devices['Desktop Chrome'] },
     },
     {
-      name: 'Firefox',
+      name: 'stage',
       use: { ...devices['Desktop Firefox'] },
     },
     {
-      name: 'WebKit',
+      name: 'prod',
       use: { ...devices['Desktop Safari'] },
     },
   ],
-  retries: 1,
-  fullyParallel: true,
+  fullyParallel: false,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env['CI'],
+  /* Retry on CI only */
+  retries: process.env['CI'] ? 1 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env['CI'] ? 2 : 1,
 });
+
