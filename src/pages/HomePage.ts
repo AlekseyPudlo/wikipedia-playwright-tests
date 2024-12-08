2ebef919a08bea4b34b4ca54517c8e624d6afbfb
Oleksii Pudlo <pudloaleksey@gmail.com>
2024-12-08 17:12:38 +0100

    feat: add HomePage class for managing homepage interactions

---
+++ src/pages/HomePage.ts
@@ -0,0 +1,29 @@
+import { BasePage } from "./BasePage";
+import { Locator } from "@playwright/test";
+
+export class HomePage extends BasePage {
+    readonly languageSelector: Locator;
+    readonly searchInput: Locator;
+    readonly searchButton: Locator;
+    
+    constructor(page: any) {
+        super(page);
+        this.languageSelector = this.page.getByRole('combobox', { name: 'Language' }) || 
+            this.page.locator('select#searchLanguage');
+        this.searchInput = this.page.locator('input[name="search"]');
+        this.searchButton = this.page.locator('button[type="submit"]');
+    }
+
+    async open(): Promise<void> {
+        await this.page.goto('/');
+    }
+
+    async setLanguage(langCode: string): Promise<void> {
+        await this.languageSelector.selectOption({ label: langCode });
+    }
+
+    async search(query: string): Promise<void> {
+        await this.searchInput.fill(query);
+        await this.searchButton.click();
+    }
+}
